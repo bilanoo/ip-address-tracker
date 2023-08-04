604a1809f2b406875b3c02583a8d36649d6af25d
bilanoo <blalkhan94@gmail.com>
2023-08-04 15:25:51 +0100

    implemented map to be displayed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "./App.css";
 
 function App() {
@@ -7,7 +8,27 @@ function App() {
         <div className="ip-address-container">
           <h1>IP Address Tracker</h1>
         </div>
-        <div className="ip-maps-container"></div>
+        <div className="ip-maps-main-container" id="map">
+          <MapContainer
+            style={{ height: "100vh" }}
+            center={[51.505, -0.09]}
+            zoom={13}
+          >
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            />
+            <Marker position={[51.505, -0.09]}>
+              <Popup>
+                <span>
+                  A pretty CSS3 popup.
+                  <br />
+                  Easily customizable.
+                </span>
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </div>
       </div>
     </>
   );
