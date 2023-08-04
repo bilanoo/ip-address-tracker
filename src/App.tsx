d5d061f494c706cf7b995d7900c05f7e26321506
bilanoo <blalkhan94@gmail.com>
2023-08-04 17:15:45 +0100

    fixed scrollign issue.

--- src/App.tsx
+++ src/App.tsx
@@ -4,31 +4,29 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="all-content-container">
-        <div className="ip-address-container">
-          <h1>IP Address Tracker</h1>
-        </div>
-        <div className="ip-maps-main-container" id="map">
-          <MapContainer
-            style={{ height: "100vh" }}
-            center={[51.505, -0.09]}
-            zoom={13}
-          >
-            <TileLayer
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            />
-            <Marker position={[51.505, -0.09]}>
-              <Popup>
-                <span>
-                  A pretty CSS3 popup.
-                  <br />
-                  Easily customizable.
-                </span>
-              </Popup>
-            </Marker>
-          </MapContainer>
-        </div>
+      <div className="ip-address-container">
+        <h1>IP Address Tracker</h1>
+      </div>
+      <div className="ip-maps-main-container" id="map">
+        <MapContainer
+          center={[51.505, -0.09]}
+          zoom={16}
+          style={{ height: "100%" }}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              <span>
+                A pretty CSS3 popup.
+                <br />
+                Easily customizable.
+              </span>
+            </Popup>
+          </Marker>
+        </MapContainer>
       </div>
     </>
   );
