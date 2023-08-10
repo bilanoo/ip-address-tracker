6d8011cb3d36f53408a68a397dfa8b44c7159e3a
bilanoo <blalkhan94@gmail.com>
2023-08-10 12:02:12 +0100

    added custom icon.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 import "./App.css";
 import { SearchIpOrDomain } from "./SearchIpOrDomain";
 import { SearchResult } from "./SearchResult";
 import { GetIpOrDomainService } from "./services/GetIpOrDomainService";
 import { useEffect, useState } from "react";
+import MarkerIcon from "./assets/images/icon-location.svg";
+import L from "leaflet";
 
 function App() {
   const [searchedAddress, setSearchAddress] = useState("");
@@ -12,11 +14,21 @@ function App() {
     searchedAddress,
   });
 
+  const newicon = new L.Icon({
+    iconUrl: MarkerIcon, // require the path to the asset
+  });
+
   useEffect(() => {
     // Fetch data once on page load
     refetch();
   }, [refetch]);
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function ChangeView({ center, zoom }: any) {
+    const map = useMap();
+    map.setView(center, zoom);
+    return null;
+  }
   return (
     <>
       <div className="ip-address-container">
@@ -31,10 +43,8 @@ function App() {
       <div className="ip-maps-main-container" id="map">
         {data && (
           <MapContainer
-            center={
-              !isLoading ? [data.latitude, data.longitude] : [51.505, -0.09]
-            }
-            zoom={16}
+            center={data ? [data.latitude, data.longitude] : [51.505, -0.09]}
+            zoom={13}
             style={{ height: "100%" }}
             zoomControl={false}
             attributionControl={false}
@@ -43,10 +53,15 @@ function App() {
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             />
+            <ChangeView
+              center={data ? [data.latitude, data.longitude] : [51.505, -0.09]}
+              zoom={13}
+            />
             <Marker
               position={
-                !isLoading ? [data.latitude, data.longitude] : [51.505, -0.09]
+                data ? [data.latitude, data.longitude] : [51.505, -0.09]
               }
+              icon={newicon}
             >
               <Popup>
                 <span>This is a rough estimate of the location.</span>
